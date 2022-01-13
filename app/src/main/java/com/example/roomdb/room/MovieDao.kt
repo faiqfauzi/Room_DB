cfe0e1e81fc3ee1868ff23a701b1edf5b8ee80e3
faiqfauzi <faiqfauzi101@gmail.com>
2022-01-13 09:02:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomdb/room/MovieDao.kt
@@ -0,0 +1,20 @@
+package com.example.roomdb.room
+
+import androidx.room.*
+
+@Dao
+interface MovieDao {
+
+    @Insert
+    suspend fun addMovie(movie: Movie)
+
+    @Update
+    suspend fun updateMovie(movie: Movie)
+
+    @Delete
+    suspend fun deleteMovie(movie: Movie)
+
+    @Query ("SELECT * FROM movie")
+    suspend fun getMovies():List<Movie>
+
+}
